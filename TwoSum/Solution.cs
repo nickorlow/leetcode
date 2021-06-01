d876651a957c65687df91f1eed4b348c5dc907a8
nickorlow <85139485+nickorlow@users.noreply.github.com>
2021-06-01 06:10:36 -0500

    Update Solution.cs

--- TwoSum/Solution.cs
+++ TwoSum/Solution.cs
@@ -1,3 +1,7 @@
+/*
+Runtime: 228 ms, faster than 97.86% of C# online submissions for Median of Two Sorted Arrays.
+Memory Usage: 32.2 MB, less than 31.14% of C# online submissions for Median of Two Sorted Arrays.
+*/
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
         for(int i = 0; i<nums.Length; i++){
